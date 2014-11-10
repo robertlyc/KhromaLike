07b0025c5212468ae24f9acdf9431a934ad851c0
robertlyc <merlinlyc@gmail.com>
2014-11-10 12:40:06 +0800

    remove deprecate method and replace to new one

--- KhromaLike/CollectionView/ColorSwatchCollectionViewController.swift
+++ KhromaLike/CollectionView/ColorSwatchCollectionViewController.swift
@@ -75,16 +75,13 @@ class ColorSwatchCollectionViewController: UICollectionViewController, ColorSwat
   }
   
   // UIViewController
-  override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-    // The orientation only makes a difference on an iphone
-    if(UIDevice.currentDevice().userInterfaceIdiom == .Phone) {
-      let newOrientation = UIApplication.sharedApplication().statusBarOrientation
-      if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
-        if newOrientation.isPortrait {
-          flowLayout.scrollDirection = .Horizontal
-        } else {
-          flowLayout.scrollDirection = .Vertical
-        }
+  override func willTransitionToTraitCollection(newCollection: UITraitCollection, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    super.willTransitionToTraitCollection(newCollection, withTransitionCoordinator: coordinator)
+    if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+      if newCollection.verticalSizeClass == .Compact {
+        flowLayout.scrollDirection = .Vertical
+      } else {
+        flowLayout.scrollDirection = .Horizontal
       }
     }
   }
